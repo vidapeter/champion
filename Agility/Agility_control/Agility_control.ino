848f48e692b6c4912b220880eae713c973789db2
bejczib <bejczi.balint@gmail.com>
2016-12-10 11:47:25 +0100

    feat: add Agility controll basic logic without compress

--- Agility/Agility_control/Agility_control.ino
+++ Agility/Agility_control/Agility_control.ino
@@ -11,16 +11,19 @@ volatile int timer = 0;
 #define SENSOR2 4
 #define MAX_CYCLE 30*4
 
+String startButton = "";
+
 static int data[MAX_CYCLE-1]; //TODO: init
 static int result[MAX_CYCLE-1];
+int faults;
 
 bool timerAction = false;
-bool isGameStarted = false;
+bool isGameOn = false;
 bool isGameOver = false;
 bool isLeftAction = false;
 bool isRightAction = false;
 
-void system_tick(){
+void systemTick(){
   if(timer < MAX_CYCLE){
     tick++;
     Timer1.restart();
@@ -28,49 +31,70 @@ void system_tick(){
   }else{
     isGameOver = true;
   }
-  
+
 }
 
-void right_action(){
+void rightAction(){
   isRightAction = true;
 }
 
-void left_action(){
+void leftAction(){
   isLeftAction = true;
+  isGameOn = false;
 }
 
 void setup() {
   //Timer setup, 250ms system tick;
   Timer1.initialize();
   Timer1.setPeriod(250000);
-  Timer1.attachInterrupt(system_tick);
+  Timer1.attachInterrupt(systemTick);
 
   pinMode(SENSOR1,INPUT);
   pinMode(SENSOR2,INPUT);
 
-  attachPCINT(digitalPinToPCINT(SENSOR1), right_action, RISING);
-  attachPCINT(digitalPinToPCINT(SENSOR2), left_action, RISING);
+  attachPCINT(digitalPinToPCINT(SENSOR1), rightAction, RISING);
+  attachPCINT(digitalPinToPCINT(SENSOR2), leftAction, RISING);
 
 }
 
 void loop() {
-  if(isGameStarted){
-
+  if(isGameOn){
 
     if(timerAction){
       if(isRightAction ^ isLeftAction){
         if(isLeftAction){
-          data[tick] = 1;
+          result[tick] = 1;
           isLeftAction = false;
         }
         if(isRightAction){
-          data[tick] = 2;
+          result[tick] = 2;
           isLeftAction = false;
         }
-      }else{data[tick] = 0;}
+      } else {result[tick] = 0;}
     }
-  
-    
+  } else if (isGameOver) {
+      isGameOver = false;
+      Serial.println("GAME OVER");
+      faults = checkFaults(data,result);
+      Serial.println("You had " + (String)(faults) + " faults.");
+  } else {
+    while (Serial.available() && isGameOn == false) {
+       startButton = (char)Serial.read();
+      if (startButton == "x") {
+        isGameOn = true;
+        Serial.println("game started.");
+      }
+    }
   }
+  delay(5);
+}
 
+int checkFaults(int data[], int result[]) {
+  int faults = 0;
+  for (int i=0;i<MAX_CYCLE;i++) {
+    if (data[i] != result[i]) {
+      faults++;
+    }
+  }
+  return faults;
 }
